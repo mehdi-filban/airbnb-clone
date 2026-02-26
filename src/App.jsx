13d926adcfbb826fcd3c0ad5cb8eb1ca1d484fc4
Mehdi Filban <137497223+mehdi-filban@users.noreply.github.com>
2026-02-26 18:00:07 +0330

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,6 @@ export default function App() {
       <Route path='/' element={<Landing />} />
       <Route path="/stays" element={<Home />} />
       <Route path="/stays/:id" element={<PropertyDetails />} />
-      <Route path="/property/:id" element={<Navigate to="/stays" replace />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
