f7e9ba87cccb8eebfc06c50de7b6c87601d0de8c
Mehdi Filban <137497223+mehdi-filban@users.noreply.github.com>
2026-02-26 16:41:28 +0330

    Enabling Github Actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/airbnb-clone/'
+  base: '/airbnb-clone/',
   plugins: [react(), tailwindcss()],
 })
